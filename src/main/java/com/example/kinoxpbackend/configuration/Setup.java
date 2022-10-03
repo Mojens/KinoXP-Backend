262410abb6b753158af5cfb0589d44b62a05d547
Mojens <moha135h@stud.kea.dk>
2022-10-03 14:59:35 +0200

    setup

---
+++ src/main/java/com/example/kinoxpbackend/configuration/Setup.java
@@ -0,0 +1,20 @@
+package com.example.kinoxpbackend.configuration;
+
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Controller;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+@Controller
+public class Setup implements ApplicationRunner {
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+
+
+
+  }
+}
