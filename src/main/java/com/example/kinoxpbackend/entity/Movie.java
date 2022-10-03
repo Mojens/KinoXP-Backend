9f75a369cd656b98ee98ae1088b670c56e3013f0
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-10-03 13:10:11 +0200

    Add entity classes for MOvie, Screening and Theater

---
+++ src/main/java/com/example/kinoxpbackend/entity/Movie.java
@@ -0,0 +1,49 @@
+package com.example.kinoxpbackend.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Builder
+@AllArgsConstructor
+
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false,length = 450)
+    private String title;
+    @Column(nullable = false,length = 450)
+    private String description;
+    @Column(nullable = false,length = 450)
+    private double rating;
+    @Column(nullable = false,length = 450)
+    private String genre;
+    @Column(nullable = false,length = 450)
+    private double duration;
+    @Column(nullable = false,length = 450)
+    private int ageLimit;
+    @Column(nullable = false,length = 450)
+    private String image;
+    @Column(nullable = false,length = 450)
+    private LocalDate showStartDate;
+    @Column(nullable = false,length = 450)
+    private LocalDate showEndDate;
+
+    @OneToMany(mappedBy = "movie")
+    List<Screening> screenings = new ArrayList<>();
+
+    public Movie() {
+    }
+
+
+
+}
---
+++ src/main/java/com/example/kinoxpbackend/entity/Screening.java
@@ -0,0 +1,41 @@
+package com.example.kinoxpbackend.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Screening {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false,length = 450)
+    private double performance;
+    @Column(nullable = false,length = 450)
+    private LocalDateTime startTime;
+    @Column(nullable = false,length = 450)
+    private LocalDateTime endTime;
+
+    @ManyToOne
+    private Movie movie;
+
+    @ManyToOne
+    private Theater theater;
+
+    @OneToMany(mappedBy = "screening")
+    private List<Reservation> reservations = new ArrayList<>();
+
+
+    public Screening() {
+    }
+
+
+}
