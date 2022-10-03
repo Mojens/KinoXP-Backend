3895ace9351f6c28fc318ce9c3bbd446674b610c
Mojens <moha135h@stud.kea.dk>
2022-10-03 17:48:58 +0200

    remove no need imports

--- src/main/java/com/example/kinoxpbackend/dto/ShiftRequest.java
+++ src/main/java/com/example/kinoxpbackend/dto/ShiftRequest.java
@@ -1,16 +1,11 @@
 package com.example.kinoxpbackend.dto;
 
-import com.example.kinoxpbackend.entity.Employee;
-import com.example.kinoxpbackend.entity.Shift;
-import com.example.kinoxpbackend.repository.EmployeeRepository;
-import com.example.kinoxpbackend.service.ShiftService;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+
 
 @Getter
 @Setter
