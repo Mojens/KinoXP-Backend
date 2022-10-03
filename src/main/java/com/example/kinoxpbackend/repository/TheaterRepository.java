f64696ea5550dcf13d72dcdaf1c78ae3972ece1d
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-10-03 13:14:45 +0200

    Add Repository classes for MOvie, Screening and Theater

---
+++ src/main/java/com/example/kinoxpbackend/repository/TheaterRepository.java
@@ -0,0 +1,9 @@
+package com.example.kinoxpbackend.repository;
+
+import com.example.kinoxpbackend.entity.Theater;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TheaterRepository extends JpaRepository<Theater, Integer> {
+
+}
+
