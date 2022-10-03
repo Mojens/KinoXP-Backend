82ebe437748f49ab86a8907777fc3d69c70cc053
Mojens <moha135h@stud.kea.dk>
2022-10-03 14:33:28 +0200

    request and response done now controller and service is needed Jens pls take over

--- src/main/java/com/example/kinoxpbackend/dto/ShiftRequest.java
+++ src/main/java/com/example/kinoxpbackend/dto/ShiftRequest.java
@@ -1,4 +1,25 @@
 package com.example.kinoxpbackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShiftRequest {
+
+  private int id;
+
+  private LocalDateTime startTime;
+
+  private LocalDateTime endTime;
+
+  private int employeeId;
+
+
 }
--- src/main/java/com/example/kinoxpbackend/dto/ShiftResponse.java
+++ src/main/java/com/example/kinoxpbackend/dto/ShiftResponse.java
@@ -1,4 +1,36 @@
 package com.example.kinoxpbackend.dto;
 
+import com.example.kinoxpbackend.entity.Shift;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ShiftResponse {
+
+  private int id;
+
+  @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss",shape = JsonFormat.Shape.STRING)
+  private LocalDateTime startTime;
+
+  @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss",shape = JsonFormat.Shape.STRING)
+  private LocalDateTime endTime;
+
+  private int employeeId;
+
+  public ShiftResponse (Shift shift, boolean includeAll){
+    this.employeeId = shift.getEmployee().getId();
+    this.startTime = shift.getStartTime();
+    this.endTime = shift.getEndTime();
+    if (includeAll){
+      this.id = shift.getId();
+    }
+  }
 }
