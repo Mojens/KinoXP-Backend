d2de45dafad02361683003e3eeb23b68e6edcd41
MBHNielsenX <mads.nielsen12@live.dk>
2022-10-06 15:25:36 +0200

    ReservationController & SeatChoiceController plus repositories have been updated

--- src/main/java/com/example/kinoxpbackend/api/ReservationController.java
+++ src/main/java/com/example/kinoxpbackend/api/ReservationController.java
@@ -3,7 +3,7 @@ package com.example.kinoxpbackend.api;
 
 import com.example.kinoxpbackend.dto.ReservationRequest;
 import com.example.kinoxpbackend.dto.ReservationResponse;
-import com.example.kinoxpbackend.entity.Reservation;
+import com.example.kinoxpbackend.dto.SeatResponse;
 import com.example.kinoxpbackend.service.ReservationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -48,5 +48,13 @@ public class ReservationController {
         reservationService.deleteReservation(id);
         return new ResponseEntity<>(true, HttpStatus.OK);
     }
+
+
+    @GetMapping("/fromScreening/{screeningId}")
+    List<SeatResponse> getSeatResponsesFromScreeningId(@PathVariable int screeningId) {
+        return reservationService.getSeatChoicesByScreening(screeningId);
+    }
+
+
 
 }
--- src/main/java/com/example/kinoxpbackend/repository/SeatChoiceRepository.java
+++ src/main/java/com/example/kinoxpbackend/repository/SeatChoiceRepository.java
@@ -1,8 +1,13 @@
 package com.example.kinoxpbackend.repository;
 
+import com.example.kinoxpbackend.dto.SeatChoiceResponse;
 import com.example.kinoxpbackend.entity.SeatChoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SeatChoiceRepository extends JpaRepository<SeatChoice, Integer> {
     SeatChoice findSeatChoiceById(int id);
+
+    List<SeatChoiceResponse> getSeatChoicesByReservationId(int id);
 }
