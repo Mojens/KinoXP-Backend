27d8ff08beef405718c0707cba2306cfd2acf582
ChineseThunder <simo09j9@stud.kea.dk>
2022-10-07 10:59:22 +0200

    html temlplates oprettet

---
+++ src/test/java/com/example/kinoxpbackend/service/ShiftServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.kinoxpbackend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShiftServiceTest {
+
+    @Test
+    void getShifts() {
+    }
+
+    @Test
+    void findShiftById() {
+    }
+
+    @Test
+    void addShift() {
+    }
+
+    @Test
+    void editShift() {
+    }
+
+    @Test
+    void deleteShiftById() {
+    }
+}
