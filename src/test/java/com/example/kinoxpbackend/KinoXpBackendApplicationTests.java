2cd41a78fe3959ca40c58db6d0748320ce587810
MBHNielsenX <mads.nielsen12@live.dk>
2022-10-10 11:36:17 +0200

    Temp fix of tests

--- src/test/java/com/example/kinoxpbackend/KinoXpBackendApplicationTests.java
+++ src/test/java/com/example/kinoxpbackend/KinoXpBackendApplicationTests.java
@@ -1,11 +1,12 @@
 package com.example.kinoxpbackend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@DataJpaTest
 class KinoXpBackendApplicationTests {
 
   @Test
